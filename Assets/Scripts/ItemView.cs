db08aa34de23d10a238af3b79bcc9a5604ffbd16
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2018-07-07 12:09:06 +0900

    選択のON/OFF機能を実装

--- Assets/Scripts/ItemView.cs
+++ Assets/Scripts/ItemView.cs
@@ -8,52 +8,59 @@ using UniRx.Triggers;
 namespace UI
 {
 	public class ItemView : MonoBehaviour
-{
-	[SerializeField] Toggle _toggle;
-	[SerializeField] private Image _activeIcon;
-	[SerializeField] private Image _inActiveIcon;
-	[SerializeField] private Text _itemText;
-	[SerializeField] private Image _backgroundImage;
-	[SerializeField] private Color _onSelectedImageColor;
-	[SerializeField] private Color _onUnSelectedImageColor;
-	[SerializeField] private Color _onSelectedTextColor;
-	[SerializeField] private Color _onUnSelectedTextColor;
-
-	[SerializeField] private BoolReactiveProperty _isSelected;
-	public IReadOnlyReactiveProperty<bool> IsSelected { get { return _isSelected; } }
-
-	public void Setup(Sprite activeIconSprite, Sprite inActiveIconSprite, string itemText)
 	{
-		_activeIcon.sprite = activeIconSprite;
-		_inActiveIcon.sprite = inActiveIconSprite;
-		_itemText.text = itemText;
 
-		_toggle.graphic = _activeIcon;
-		_toggle.group = GetComponentInParent<ToggleGroup>();
+		[SerializeField] private Button button;
+		public Button ItemButton { get { return button; } }
+		[SerializeField] private Image iconImage;
+		[SerializeField] private Text itemText;
+		[SerializeField] private Image backgroundImage;
 
-		_toggle.onValueChanged.AddListener(_ => OnvalueChanged());
+		[SerializeField] private Color selectedImageColor;
+		[SerializeField] private Color unSelectedImageColor;
+		[SerializeField] private Color selectedTextColor;
+		[SerializeField] private Color unSelectedTextColor;
 
-		
-	}
+		private Sprite activeSprite;
+		private Sprite inActiveSprite;
 
-	private void OnvalueChanged()
-	{
-		if(_toggle.isOn)
+		private bool _onSelected;
+		public bool OnSelected { get { return _onSelected; } }
+
+		public void Setup(Sprite activeSprite, Sprite inActiveSprite, string itemText)
 		{
-			_backgroundImage.color = _onSelectedImageColor;
-			_itemText.color = _onSelectedTextColor;
-
-		}
-		else
-		{
-			_backgroundImage.color = _onUnSelectedImageColor;
-			_inActiveIcon.color = _onUnSelectedImageColor;
-			_itemText.color = _onUnSelectedTextColor;
+			this.activeSprite = activeSprite;
+			this.inActiveSprite = inActiveSprite;
+			this.iconImage.sprite = inActiveSprite;
+			this.itemText.text= itemText;
+			_onSelected = false;
 		}
 
-		_isSelected.Value = _toggle.isOn;
+		public void Selected()
+		{
+			iconImage.sprite = activeSprite;
+			iconImage.color = Color.white;
+			itemText.color = selectedTextColor;
+			backgroundImage.color = selectedImageColor;
+			_onSelected = true;
+		}
 
-		
+		public void Unselected()
+		{
+			iconImage.sprite = inActiveSprite;
+			iconImage.color = unSelectedImageColor;
+			itemText.color = unSelectedTextColor;
+			backgroundImage.color = unSelectedImageColor;
+			_onSelected = false;
+		}
+
+		public void Default()
+		{
+			iconImage.sprite = inActiveSprite;
+			iconImage.color = Color.white;
+			itemText.color = Color.black;
+			backgroundImage.color = Color.white;
+			_onSelected = false;
+		}
 	}
-}
 }
---
+++ Assets/Scripts/ItemViewPresenter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace UI
+{
+	public class ItemViewPresenter : MonoBehaviour
+	{
+		List<ItemView> itemList = new List<ItemView>();
+		[SerializeField] GameObject itemViewPrefab;
+		[SerializeField] Image arrow;
+		[SerializeField] Color onSelectedArrowColor;
+		[SerializeField] private Sprite activeSprite;
+		[SerializeField] private Sprite inActiveSprite;
+		[SerializeField] int itemNum;
+
+		void Awake()
+		{
+			for(int i = 0; i < itemNum; i++)
+			{
+				var itemObj = Instantiate(itemViewPrefab);
+				itemObj.transform.SetParent(transform, false);
+				var itemView = itemObj.GetComponent<ItemView>();
+
+				itemView.Setup(activeSprite, inActiveSprite, "Items");
+				itemView.Default();
+				itemList.Add(itemView);
+
+				itemView.ItemButton.OnClickAsObservable().Subscribe(_ =>
+				{
+					ViewLogic(itemView);
+				});
+			}
+		}
+
+		private void ViewLogic(ItemView itemView)
+		{
+			if(!itemView.OnSelected)
+			{
+				itemView.Selected();
+				foreach(var item in itemList.FindAll(item => item != itemView))
+				{
+					item.Unselected();
+				}
+				arrow.color = onSelectedArrowColor;
+			}
+			else
+			{
+				foreach(var item in itemList)
+				{
+					item.Default();
+				}
+				arrow.color = Color.white;
+			}
+		}
+	}
+}
